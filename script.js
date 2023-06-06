b6510f6ebc2a85ce29a8a39f8e32f891594aca38
rogercsar <rogercsardev@gmail.com>
2023-06-06 12:15:14 -0400

    update css and html

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function toggleMode() {
 
     const currentTheme = localStorage.getItem('rogerlink:theme')
 
-
     if (currentTheme === 'light') {
         localStorage.setItem('rogerlink:theme', 'dark')
         html.classList.remove('light')
@@ -15,7 +14,6 @@ function toggleMode() {
         html.classList.add('light')
     }
     
-
     //Pegar tag img
     const img = document.querySelector("#profile img")
 
@@ -29,11 +27,35 @@ function toggleMode() {
 
 document.addEventListener('DOMContentLoaded', () => {
     const currentTheme = localStorage.getItem('rogerlink:theme')
+    const gnome1 = localStorage.getItem('nome1')
+    const glink1 = localStorage.getItem('link1')
+    let btn1 = document.querySelector("#btn1")
+
+    const gnome2 = localStorage.getItem('nome2')
+    const glink2 = localStorage.getItem('link2')
+    let btn2 = document.querySelector("#btn2")
+
+    const gnome3 = localStorage.getItem('nome3')
+    const glink3 = localStorage.getItem('link3')
+    let btn3 = document.querySelector("#btn3")
+
+    const gnome4 = localStorage.getItem('nome4')
+    const glink4 = localStorage.getItem('link4')
+    let btn4 = document.querySelector("#btn4")
     
     if (currentTheme === 'light') document.documentElement.classList.add('light')
     if (currentTheme === 'dark') document.documentElement.classList.remove('light')
 
-    document.getElementById("btn1").setAttribute("href", localStorage.getItem('rogerlink:link1'))  
-})
+    btn1.innerHTML = gnome1   
+    btn1.setAttribute("href", glink1)
 
+    btn2.innerHTML = gnome2
+    btn2.setAttribute("href", glink2)
+
+    btn3.innerHTML = gnome3
+    btn3.setAttribute("href", glink3)
+
+    btn4.innerHTML = gnome4
+    btn4.setAttribute("href", glink4)
+})
 
